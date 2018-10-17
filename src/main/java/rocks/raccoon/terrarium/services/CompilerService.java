b7b834a3199bc7f975da240dd8ba819ab3492130
Artem Korneev <artemkorneev@users.noreply.github.com>
2018-10-16 22:55:31 -0700

    Turning supplier to function with map parameter

--- src/main/java/rocks/raccoon/terrarium/services/CompilerService.java
+++ src/main/java/rocks/raccoon/terrarium/services/CompilerService.java
@@ -1,28 +1,36 @@
 package rocks.raccoon.terrarium.services;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 import net.openhft.compiler.CompilerUtils;
-
-import java.util.function.Supplier;
 
 public class CompilerService {
     public String foo() {
         String result = "default value";
         String className = "mypackage.MyClass";
         String javaCode2 = "package mypackage;\n" +
-                "import java.util.function.Supplier;\n" +
-                "public class MyClass implements Supplier<String> {\n" +
-                "    public String get() {\n" +
-                "        return \"Hello compiled supplier World\";" +
+                "import java.util.function.Function;\n" +
+                "import java.util.Map;\n" +
+                "public class MyClass implements Function<Map<String, String>, String> {\n" +
+                "    public String apply(Map<String, String> values) {\n" +
+                "        return \"Hello compiled supplier World\" + values;" +
                 "    }\n" +
                 "}\n";
         try {
             Class aClass = CompilerUtils.CACHED_COMPILER.loadFromJava(className, javaCode2);
-            Supplier<String> runner = (Supplier<String>) aClass.newInstance();
-            result = runner.get();
+
+            Function<Map<String, String>, String> func = (Function<Map<String, String>, String>) aClass.newInstance();
+            Map<String, String> values = new HashMap<>();
+            values.put("a-a-a", "b-b-b");
+            values.put("c-c-c", "d-d-d");
+
+            result = func.apply(values);
+
             System.out.println(result);
         } catch (Exception e) {
-
         }
+
         return result;
     }
 }
